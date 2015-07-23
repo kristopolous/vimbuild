d7f97ce13a9ca2dec16c7e3899a74b27437a26d5
Chris McKenzie <kristopolous@yahoo.com>
2015-07-23 03:14:04 -0700

    backing up config JIC and also making sure that we deprecate

--- easyinstall.sh
+++ easyinstall.sh
@@ -3,6 +3,12 @@
 . common.sh
 INSTALLDIR=~/.vim/
 
+backup=`mktemp -d`
+
+echo "Using $mydir as the place"
+[ -e $INSTALLDIR ] && mv $INSTALLDIR $backup
+mv ~/.vimrc $backup
+
 _mkdir $INSTALLDIR
 tar xzvf config/dotvim.tar.gz -C $INSTALLDIR
 
